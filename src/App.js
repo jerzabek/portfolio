eb638fc4a473e1bb9a93b794f7a13206824bdeb6
Ivan <jerzabek.ivan@gmail.com>
2020-11-01 13:18:38 +0100

    Added Bootstrap and edited the page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="container h-100 text-center d-flex flex-column align-items-center justify-content-center">
+        <div className="my-5">
+          <h1>Hello world!</h1>
+          <h2 className="display-1">Ivan Jeržabek</h2>
+          <h2 className="text-danger">This page is work in progress!</h2>
+        </div>
+      </div>
+    </main>
   );
 }
 
