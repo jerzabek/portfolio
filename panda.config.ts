f07a3154baaa51fbb0df9943a6724127e05e7592
Ivan Jerzabek <jerzabek.ivan@gmail.com>
2025-11-17 20:30:03 +0100

    Svelte app & panda css setup

---
+++ panda.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from '@pandacss/dev';
+
+export default defineConfig({
+	preflight: true,
+
+	include: ['./src/**/*.{js,ts,svelte}'],
+
+	exclude: [],
+
+	theme: {
+		extend: {}
+	},
+
+	outdir: 'styled-system'
+});
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { css } from 'styled-system/css';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class={css({ fontSize: '2xl', fontWeight: 'bold' })}>Hello 🐼!</div>
